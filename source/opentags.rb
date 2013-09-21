e859462f0b2bb636c85bc976209b722499c4b37f
Jay Zawrotny <jayzawrotny@gmail.com>
2013-09-21 17:04:35 -0400

    Added in comments to code. Because professionalism.

--- source/opentags.rb
+++ source/opentags.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
+###############################################################################
+# Name: HTML Open Tag Finder
+# Purpose: Finds unclosed HTML tags and shows them & line # on console. 
+# Author: Jay Zawrotny <jayzawrotny@gmail.com>
+# Date: September 21, 2013
+# Version: 0.1
+###############################################################################
+# Requirements:
+#   Ruby 2.0+
+###############################################################################
+# Notes:
+#   If complexity grows, will be good to switch to Thor for arguments and
+#   building.
+#   TODO: Don't forget to refactor into classes!
+###############################################################################
 require 'optparse'
 require 'ostruct'
 
-options = OpenStruct.new
-options.htmlFiles = []
+options = OpenStruct.new  # Contains our options in a hash-like object.
+options.htmlFiles = []  # Contains an array of filenames to read.
 html = []
 
 OptionParser.new do |opts|
